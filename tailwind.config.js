25749610cca19d485341a5b6637003d1d05087e9
AnupriyaKumari <anupriya10kumari@gmail.com>
2022-01-20 00:06:50 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  purge:[
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'Arial', 'sans-serif']
+      },
+      spacing: {
+        '14': '3.5rem',
+        '18': '4.5rem',
+        '68': '16rem',
+        '92': '23rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
